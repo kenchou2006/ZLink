7933a0f4bba9d5943b881e8bdf65c2ab65a61da5
周恭煥 <65105273+kenchou2006@users.noreply.github.com>
2026-01-25 19:15:40 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 python3 -m pip install uv
-python3 -m uv pip install -r requirements.txt
 
-python3 -m uv manage.py migrate
-python3 -m uv manage.py collectstatic --noinput
+uv pip install -r requirements.txt
+uv manage.py migrate
+uv manage.py collectstatic --noinput
